8a561476b9d14cbc165c401cd70da3b87e56967f
Eugene <Eugene@192.168.0.101>
2015-01-23 22:40:20 +0300

    Add cicl for

--- Lesson 20 Git-test-project/src/ru/baloven82/test/Main.java
+++ Lesson 20 Git-test-project/src/ru/baloven82/test/Main.java
@@ -2,7 +2,9 @@ package ru.baloven82.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world, this is Git Hub");
+		for (int i = 0; i<4; i++) {
+			System.out.println("Hello world, this is Git Hub");
+		}
 	}
 
 	
